a2628476e72f843d7315319cbcee5e7dbe5181fb
RAMCHARAN <ramcharankhv@gmail.com>
2025-11-01 10:08:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let output = document.getElementById("Display-Result");
+let input = document.getElementById("input");
+let sbmt = document.getElementById("Submit");
+
+
+const password_gen = (password_length, isLowerCase, isUpperCase, isNumber, isSpecial) => {
+    let password = "";
+    let allowedchars = "";
+    const lc = "abcdefghijklmnopqrstuvwxyz";
+    const uc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const num = "0123456789";
+    const special = "!@#$%^&*_-+/";
+
+
+    allowedchars += isLowerCase ? lc : "";
+    allowedchars += isUpperCase ? uc : "";
+    allowedchars += isNumber ? num : "";
+    allowedchars += isSpecial ? special : "";
+
+    for (i = 0; i < password_length; i++) {
+        password += allowedchars[Math.floor(Math.random() * allowedchars.length)];
+    }
+    return password;
+}
+
+
+    sbmt.addEventListener("click", () => {
+    let password_length = Number(input.value);
+    let isLowerCase = document.getElementById("Lowercase").checked;
+    let isUpperCase = document.getElementById("Uppercase").checked;
+    let isNumber = document.getElementById("Number").checked;
+    let isSpecial = document.getElementById("special").checked;
+    
+    if(password_length>15)
+    {
+        output.textContent = "Enter number less than 15";
+        return;
+    }
+
+    // ✅ Check if none of them are selected
+    if (!isLowerCase && !isUpperCase && !isNumber && !isSpecial) {
+        output.textContent = "Check at least one box to generate!";
+        return; // stop execution here
+    }
+
+    output.textContent = `Password = ${password_gen(password_length, isLowerCase, isUpperCase, isNumber, isSpecial)}`;
+});
+
