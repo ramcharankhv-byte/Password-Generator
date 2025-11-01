9a54e561bf942c0fa186bec299fa789957986a6b
RAMCHARAN <ramcharankhv@gmail.com>
2025-11-01 23:25:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let output = document.getElementById("Display-Result");
 let input = document.getElementById("input");
 let sbmt = document.getElementById("Submit");
+const lc = "abcdefghijklmnopqrstuvwxyz";
+const uc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+  
 
 const password_gen = (password_length, isLowerCase, isUpperCase, isNumber, isSpecial) => {
     let password = "";
@@ -25,18 +28,26 @@ const password_gen = (password_length, isLowerCase, isUpperCase, isNumber, isSpe
 
 
     sbmt.addEventListener("click", () => {
+
+    if(/[a-zA-Z]/.test(input.value))
+    {
+        output.textContent = "Should not contain letters"
+    }
     let password_length = Number(input.value);
     let isLowerCase = document.getElementById("Lowercase").checked;
     let isUpperCase = document.getElementById("Uppercase").checked;
     let isNumber = document.getElementById("Number").checked;
     let isSpecial = document.getElementById("special").checked;
-    
+    if(password_length<0)
+    {
+        output.textContent = "Password length must be greater than zero!"
+    }
     if(password_length>15)
     {
         output.textContent = "Enter number less than 15";
         return;
     }
-
+    
     // ✅ Check if none of them are selected
     if (!isLowerCase && !isUpperCase && !isNumber && !isSpecial) {
         output.textContent = "Check at least one box to generate!";
